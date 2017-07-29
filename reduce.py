1242878870a1b934e02e32003961d3dfe1617241
Rahul P Shah <rshah5@ncsu.edu>
2017-07-29 18:47:41 -0400

    Update reduce.py

--- reduce.py
+++ reduce.py
@@ -1,5 +1,19 @@
 from collections import defaultdict
+
+
+'''
+lst: List of tuples of the form [(k, v1), (k, v2)]. The reduce changes to {k:[v1, v2]}
+'''
 def reduce(lst):
-	res = defaultdict(list)
-	for k,v in lst: res[k].append(v)
-	return dict(res)
+        res = defaultdict(list)
+        for k,v in lst: res[k].append(v)
+        return dict(res)
+
+'''
+Gets the freq_count of all the items in the list
+'''
+def freq_count(lst):
+        dct = defaultdict(int)
+        for item in lst:
+                dct[item] += 1
+        return dict(dct)
